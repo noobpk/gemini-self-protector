f54ca881a73f287301ea85bb64d631eab6d89b84
noobpk <ltp.noobpk@gmail.com>
2023-10-05 00:04:25 +0700

    update tests

--- gemini-python/gemini_self_protector/tests/test_gemini_self_protector_cli.py
+++ gemini-python/gemini_self_protector/tests/test_gemini_self_protector_cli.py
@@ -5,25 +5,29 @@ app = Flask(__name__)
 
 gemini = GeminiManager()
 
+
 @app.route("/")
 @gemini.flask_protect_extended()
-def hello_world():
-    return "<p>Basic Config!</p>"
+def index():
+    return "<p>Running gemini-self protector - CLI Mode!</p>"
+
 
 @app.route("/protection")
 @gemini.flask_protect_extended(protect_mode='protection')
-def hello_world():
-    return "<p>Specific Config - Protection Mode!</p>"
+def protection_mode():
+    return "<p>Protect mode - Protection!</p>"
+
 
 @app.route("/monitor")
 @gemini.flask_protect_extended(protect_mode='monitor')
-def hello_world():
-    return "<p>Specific Config - Monitor Mode!</p>"
+def monitor_mode():
+    return "<p>Protect mode - Monitor!</p>"
+
 
 @app.route("/off")
 @gemini.flask_protect_extended(protect_mode='off')
-def hello_world():
-    return "<p>Specific Config - Off Mode!</p>"
+def off_mode():
+    return "<p>Protect mode - Off!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
